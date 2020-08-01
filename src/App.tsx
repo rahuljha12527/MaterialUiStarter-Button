7e11564b6c6fb115bc3caf25215722049c41b020
rahuljha661 <rahuljha12527@gmail.com>
2020-08-01 22:09:34 +0530

    material ui two button completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+import "./App.css";
+
+//import Button from "@material-ui/core/Button";
+import WhatsAppIcon from "@material-ui/icons/WhatsApp";
+// import FacebookIcon from '@material-ui/icons/Facebook';
+import { makeStyles } from "@material-ui/core/styles";
+import IconButton from "@material/react-icon-button";
+import PictureAsPdfSharpIcon from '@material-ui/icons/PictureAsPdfSharp';
+
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+    marginRight:'2%',
+    
+  },
+  label: {
+    width: '100%',
+    display: 'flex',
+    flexDirection:'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  
+  },
+  
+ 
+});
 function App() {
+
+ 
+  const classes = useStyles();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+        
+     
+      <IconButton className={classes.root  }    color="secondary" aria-label="add an alarm"  >
+  <span className={classes.label}>Share &nbsp;  <WhatsAppIcon />  </span>
+    
+      </IconButton>
+      <IconButton  className={classes.root} color="secondary" aria-label="add an alarm">
+      <span className={classes.label}>Download &nbsp;  <PictureAsPdfSharpIcon/>  </span>
+      
+</IconButton>
+      
     </div>
   );
 }
